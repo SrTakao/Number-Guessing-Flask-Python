8c8d89355a5f4476b7dae4e8ff209d3c840a2ecb
SrTakao <rafaeltakao9@gmail.com>
2025-10-01 14:30:40 -0400

    commit 02

--- application/main.py
+++ application/main.py
@@ -1,7 +1,9 @@
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf.csrf import CSRFProtect
 from models import Usuario
 from db import datab
+from random import randint
 import hashlib
 
 app = Flask(__name__)
@@ -10,6 +12,8 @@ lm = LoginManager(app)
 lm.login_view = 'login'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 datab.init_app(app)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 def hash(txt):
     hash_obj = hashlib.sha256(txt.encode('utf-8'))
@@ -66,7 +70,27 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-#--# inicia o codigo pagian inicial
+@app.route('/game', methods=['GET', 'POST'])
+@login_required
+def game():
+    numSecret = randint(1,100)
+    palpite = 0 
+    resultado = ''
+    if request.method == 'GET':
+        return render_template('game.html')
+    elif request.method == 'POST': 
+        palpite = request.form['guess']
+        if palpite == numSecret:
+            resultado = 'Numero Correto! :D'
+        else:
+            resultado = f'Numero Errado! :( \nO resultado era:{numSecret}'
+        
+    return render_template('game.html', resposta = resultado)
+        
+
+
+
+#--# inicia o codigo pagina inicial
 if __name__ == "__main__":
     with app.app_context():
         datab.create_all()
